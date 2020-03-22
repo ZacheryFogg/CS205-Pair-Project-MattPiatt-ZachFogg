b171c857452ff2aa66c953d21305ead3cf6dbe69
Zachery Fogg <z.fogg1234@gmail.com>
2020-03-22 10:29:19 -0400

    Updated Files

--- Course.py
+++ Course.py
@@ -17,37 +17,71 @@ class Course:
 	# can change the course title, credits, or sections through public methods
 
 	def setCredits(self, creditValue):
-		self.credits = credit_value
+		self.credits = creditValue
 
 	def setCourseTitle(self, newTitle):
 		self.courseTitle = newTitle
 
-	def addSection(self, section):
-		sections.append(section)
-		sections = list(set(section)) # removes potential duplicate sections
+	def addSection(self, sectionObj=None):
+
+		if (sectionObj is not None): #add section that has been made.
+			if (isinstance(sectionObj, Section.section )): # Make sure the object being passed is of correct type
+				self.sections.append(sectionObj)
+
+		else:
+			#add section that also needs to be created
+			professor = input("\nEnter class professor's name: ")
+			section = input("\nEnter section: ")
+			time = input("\nEnter class start time: ")
+			duration = input("\nEnter class duration: ")
+			location = input("\nEnter class location: ")
+			enrollment = input ("\nEnter class enrollment: ")
+
+			sectionObj = Section.section(professor, section, time, duration, location, enrollment)
+			self.sections.append(sectionObj)
+
+
 
 	def removeSection(self, section):
-		sections.remove(section)
+		self.sections.remove(section)
 
 	def displayCourseInfo(self):
-		print("\n\nCourse title: " + self.courseTitle +"\n" + "Credits: " + str(self.credits) + "\n"
-			"Sections: " + str(self.sections)+"\n\n")
+		returnString = "\n\nCourse title: " + self.courseTitle +"\n" + "Credits: " + str(self.credits) + "\nSections: "
+		for section in self.sections:
+			returnString += str(section) + ", "
+		returnString = returnString[:-2]
+		print(returnString)
+	def getCredits(self):
+		return self.credits
 
-	#def get_credits(self):
-	#	print(str(credits))
-		pass
 	def getCourseTitle(self):
 		#print("Course title: " + self.courseTitle)
 		return self.courseTitle
 
 	def getSections(self):
-		print("Sections: " + str(self.sections))
+		#print("Sections: " + str(self.sections))
 		return self.sections
 
-#def main():
-	#test_course = Course("Random",3)
-	#test_course.get_credits()
-	#test_course.displayCourseInfo()
+	def displaySection(self, sectionChar):
+		for section in self.sections:
+
+			if (section.section == sectionChar.upper()): # found desired section
+				section.displaySectionInformation()
+				return
+
+		print("\n\nsection not found, make sure to specify unique section character\n\n")
+
+	def displayAllSections(self):
+		for section in self.sections:
+			section.displaySectionInformation()
 
 
-#main()
+
+# def main():
+# 	course1 = Course("default",3)
+# 	course1.addSection()
+# 	course1.displayCourseInfo()
+# 	course1.displaySection("A")
+# 	course1.displaySection("B")
+
+# main()
